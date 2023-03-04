c49eec184e1c5cedad5b084ad72ba87b8c1e63c8
OmerAzazi <98057647+OmerAzazi@users.noreply.github.com>
2023-03-04 08:37:14 -0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,72 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <br><br>
+            <img src="home/images/logo.png" style="width:60px;  display: block;
+            margin-left: auto;
+            margin-right: auto;">
+            <span style="padding-left:530px;font-size:20px"> Why I<span style="color:red">C</span>hatInstant ?</span><br>
+            <span style="padding-left:540px;font-size:20px">9 Reasons Why..</span><br><br>
+            &nbsp01.
+            Live Chat is #1
+            Chatify makes offering amazing live chat support possible, by seamlessly integrating with your website. It's an easy and cost effective way to support your customers right when they need it.<br><br>
+
+
+            &nbsp02.
+            Q&A's Add Depth
+            Ever find yourself scrolling to the customer Questions & Answers section on an Amazon product page that you're trying to talk yourself out of buying?
+
+            Well you're nost alone. Adding a dynamic Q&A section to your product pages is a fantastic way to continually enhance the information on the page.<br><br>
+
+            &nbsp03.
+            Free means FREE
+            Our competitors use 'Feature Gating' to force upgrades to higher plans. Their "free plans" are really just 'feature free'. We really don't believe in feature gating. That's why all our plans enjoy the same access to our core features which includes our FREE plan.<br><br>
+
+
+            &nbsp04.
+            The fairest way to charge
+            Our larger competitors come with 'per-agent' or even worse 'per contact' fees. This means that the bigger your team or customer list, the more you pay.
+
+            For our paid account types, pricing is based on conversations so that you can have your entire team on Chatfiy, without incurring additional costs.<br><br>
+
+
+            &nbsp05.
+            It's all about Teamwork
+            We designed Chatify to make it easy for teams to work together on customer queries. You can assign queries to colleagues or groups, or even discuss individual queries internally, without having to leave the app. It really does simplify customer support.
+
+            Remember it's free to add as many team members as you want. Team Messaging is one of our most productive features.<br><br>
+
+
+            &nbsp06.
+            Easy to Use, wherever you are
+            Welcome to mobile first customer support. Chatify inlcudes a number of features (ex. Agent Assist, Persistence, etc.) that means, you really can support your customers from your smartphone.
+
+            With dedicated Desktop and Mobile apps, Chatify is really simple to use in the office or on the move.<br><br>
+
+
+            &nbsp07.
+            Right Message, Right Team Member
+            Serving the correct message at the correct time, will increase conversions. Likewise, setting well designed rules to automatically assign chats based on keywords or urls, will significantly increase support efficiency.<br><br>
+
+
+            &nbsp08.
+            Chat Live & Reply Later
+            Some chats are synchronous (live), others are asynchronous (reply later). This blend works best for you and your customers.<br><br>
+
+
+            &nbsp09.
+            Around the clock support
+            The benefits of artificial intelligence are real. With Chatify, AI is used to automatically answer repetitive questions but also to help your team, by suggesting possible answers, as they chat with customers.<br><br>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
